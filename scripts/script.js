4d143485ec347ee2c973af9064d54aab4bbb30b6
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2024-10-22 16:17:17 -0700

    set up game object literal

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+const game = {
+  title: 'Green Mode',
+  isRunning: false,
+  gameBoard: document.querySelector('.game-board'),
+  scoreBoard: document.querySelector('.score-board'),
+  playerForm: document.querySelector('.player-form'),
+  joinGameButton: document.querySelector('#start-game-button'),
+  scorePointsButton: document.querySelector('#score-points-button'),
+  playerNameDisplay: document.querySelector('.player-name-display'),
+  playerScoreDisplay: document.querySelector('.player-score-display'),
+};
