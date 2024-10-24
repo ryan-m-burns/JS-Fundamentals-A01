86e47c7e0fa80259dff15a5a5fdafe05d9358b7d
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2024-10-23 17:12:03 -0700

    fully functional pink-mode

--- scripts/script.js
+++ scripts/script.js
@@ -1,68 +1,94 @@
 'use strict';
 
-(function (d) {
-  const game = {
-    title: 'Green Mode',
-    isRunning: false,
+const game = {
+  title: 'Pink Mode',
+  isRunning: false,
+  players: [],
+  activePlayer: null,
+  gameBoard: document.querySelector('#game-board'),
+  scoreBoard: document.querySelector('#score-board'),
+  playerForm: document.querySelector('.player-form'),
+  joinGameButton: document.querySelector('#join-game-button'),
+  startGameButton: document.querySelector('#start-game-button'),
+  switchPlayerButton: document.querySelector('#switch-player-button'),
+  scorePointsButton: document.querySelector('#score-points-button'),
+  body: document.querySelector('body'),
 
-    gameBoard: d.querySelector('#game-board'),
-    scoreBoard: d.querySelector('#score-board'),
-    playerForm: d.querySelector('.player-form'),
-    joinGameButton: d.querySelector('#join-game-button'),
-    startGameButton: d.querySelector('#start-game-button'),
-    scorePointsButton: d.querySelector('#score-points-button'),
-    playerNameDisplay: d.querySelector('.player-name-display'),
-    playerScoreDisplay: d.querySelector('.player-score-display'),
+  createAndAppendElement(tag, className, textContent) {
+    const element = document.createElement(tag);
+    if (className) element.className = className;
+    if (textContent) element.textContent = textContent;
+    return element;
+  },
 
-    toggleRunning() {
-      this.isRunning = !this.isRunning;
-      if (this.isRunning) {
-        this.startGameButton.textContent = 'Pause';
-        this.scorePointsButton.disabled = false;
-        d.querySelector('body').classList.remove('paused');
-        this.scoreBoard.classList.remove('greyed');
-      } else {
-        this.startGameButton.textContent = 'Resume';
-        this.scorePointsButton.disabled = true;
-        d.querySelector('body').classList.add('paused');
-        this.scoreBoard.classList.add('greyed');
+  addPlayer(playerObj) {
+    const playerIndex = this.players.length;
+    this.players.push(playerObj);
+
+    const div = this.createAndAppendElement('div', `player${playerIndex}`);
+    const nameP = this.createAndAppendElement('p', `player${playerIndex}-name`, playerObj.name);
+    const scoreP = this.createAndAppendElement(
+      'p',
+      `player${playerIndex}-score`,
+      playerObj.score ? playerObj.score : '0'
+    );
+    div.append(nameP, scoreP);
+    this.scoreBoard.append(div);
+  },
+
+  updatePlayerScore(playerIndex) {
+    const randomNumber = Math.ceil(Math.random() * 10);
+    let player = this.players[playerIndex];
+    player.updatePlayerScore(randomNumber);
+    this.scoreBoard.querySelector(`.player${playerIndex}-score`).textContent = player.score;
+  },
+
+  toggleGameMode() {
+    this.playerForm.hidden = true;
+    this.activePlayer = this.activePlayer ? this.activePlayer : 0;
+    this.scoreBoard.querySelector(`.player${this.activePlayer}`).classList.add('featured');
+    this.isRunning = !this.isRunning;
+    this.startGameButton.textContent = this.isRunning ? 'Pause' : 'Resume';
+    this.scorePointsButton.disabled = !this.isRunning;
+    this.switchPlayerButton.disabled = !this.isRunning;
+    this.body.classList.toggle('paused', !this.isRunning);
+    this.scoreBoard.classList.toggle('greyed', !this.isRunning);
+  },
+
+  switchActivePlayer() {
+    this.activePlayer = this.activePlayer + 1 >= this.players.length ? 0 : this.activePlayer + 1;
+    this.scoreBoard.querySelector('.featured').classList.remove('featured');
+    this.scoreBoard.querySelector(`.player${this.activePlayer}`).classList.add('featured');
+  },
+
+  init() {
+    this.playerForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const playerName = document.getElementById('player-name').value;
+      if (playerName) {
+        this.addPlayer(new player(playerName));
+        if (this.players.length > 1) this.startGameButton.disabled = false;
       }
-    },
+    });
+    this.startGameButton.addEventListener('click', () => this.toggleGameMode());
+    this.scorePointsButton.addEventListener('click', () =>
+      this.updatePlayerScore(this.activePlayer)
+    );
+    this.switchPlayerButton.addEventListener('click', () => this.switchActivePlayer());
+  }
+};
 
-    updatePlayerName(playerName) {
-      this.playerNameDisplay.textContent = playerName;
-    },
+class player {
+  constructor(name) {
+    this.name = name;
+    this.score = 0;
+  }
 
-    updatePlayerScore(currentScore) {
-      this.playerScoreDisplay.textContent = currentScore;
-    }
-  };
+  updatePlayerScore(addToScore) {
+    this.score += addToScore;
+  }
+}
 
-  const player = {
-    name: 'Player 0',
-    score: 0,
-
-    updatePlayerName() {
-      this.name = d.getElementById('player-name').value;
-      console.log(this.name);
-      game.updatePlayerName(this.name);
-    },
-
-    updatePlayerScore() {
-      this.score += 2;
-      game.updatePlayerScore(this.score);
-    }
-  };
-
-  game.joinGameButton.addEventListener('click', () => {
-    if (d.getElementById('player-name').value) {
-      player.updatePlayerName();
-      game.joinGameButton.disabled = true;
-      game.playerForm.hidden = true;
-      game.startGameButton.disabled = false;
-    }
-  });
-
-  game.startGameButton.addEventListener('click', () => game.toggleRunning());
-  game.scorePointsButton.addEventListener('click', () => player.updatePlayerScore());
-})(document);
+document.addEventListener('DOMContentLoaded', () => {
+  game.init();
+});
